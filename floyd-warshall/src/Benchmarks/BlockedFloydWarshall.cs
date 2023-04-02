cb2b45463d34225ac96f568a6f81bab91af14ba2
OlegKarasik <olegkarasik@coherentsolutions.com>
2023-04-03 01:25:35 +0300

    Updated files.

--- floyd-warshall/src/Benchmarks/BlockedFloydWarshall.cs
+++ floyd-warshall/src/Benchmarks/BlockedFloydWarshall.cs
@@ -22,7 +22,7 @@ namespace Code.Benchmarks
       };
       var blocks = new[]
       {
-        30
+        50
       };
 
       return inputs
@@ -102,146 +102,146 @@ namespace Code.Benchmarks
       }
     }
 
-    // + graph specific optimization
-    [Benchmark]
-    [ArgumentsSource(nameof(Arguments))]
-    public void FloydWarshall_01(int[] matrix, int sz)
-    {
-      for (var k = 0; k < sz; ++k)
-      {
-        for (var i = 0; i < sz; ++i)
-        {
-          if (matrix[i * sz + k] == Constants.NO_EDGE)
-          {
-            continue;
-          }
-          for (var j = 0; j < sz; ++j)
-          {
-            var distance = matrix[i * sz + k] + matrix[k * sz + j];
-            if (matrix[i * sz + j] > distance)
-            {
-              matrix[i * sz + j] = distance;
-            }
-          }
-        }
-      }
-    }
+    // // + graph specific optimization
+    // [Benchmark]
+    // [ArgumentsSource(nameof(Arguments))]
+    // public void FloydWarshall_01(int[] matrix, int sz)
+    // {
+    //   for (var k = 0; k < sz; ++k)
+    //   {
+    //     for (var i = 0; i < sz; ++i)
+    //     {
+    //       if (matrix[i * sz + k] == Constants.NO_EDGE)
+    //       {
+    //         continue;
+    //       }
+    //       for (var j = 0; j < sz; ++j)
+    //       {
+    //         var distance = matrix[i * sz + k] + matrix[k * sz + j];
+    //         if (matrix[i * sz + j] > distance)
+    //         {
+    //           matrix[i * sz + j] = distance;
+    //         }
+    //       }
+    //     }
+    //   }
+    // }
 
-    // + graph specific optimization
-    // + parallel
-    [Benchmark]
-    [ArgumentsSource(nameof(Arguments))]
-    public void FloydWarshall_02(int[] matrix, int sz)
-    {
-      for (var k = 0; k < sz; ++k)
-      {
-        Parallel.For(0, sz, i =>
-        {
-          if (matrix[i * sz + k] == Constants.NO_EDGE)
-          {
-            return;
-          }
-          for (var j = 0; j < sz; ++j)
-          {
-            var distance = matrix[i * sz + k] + matrix[k * sz + j];
-            if (matrix[i * sz + j] > distance)
-            {
-              matrix[i * sz + j] = distance;
-            }
-          }
-        });
-      }
-    }
+    // // + graph specific optimization
+    // // + parallel
+    // [Benchmark]
+    // [ArgumentsSource(nameof(Arguments))]
+    // public void FloydWarshall_02(int[] matrix, int sz)
+    // {
+    //   for (var k = 0; k < sz; ++k)
+    //   {
+    //     Parallel.For(0, sz, i =>
+    //     {
+    //       if (matrix[i * sz + k] == Constants.NO_EDGE)
+    //       {
+    //         return;
+    //       }
+    //       for (var j = 0; j < sz; ++j)
+    //       {
+    //         var distance = matrix[i * sz + k] + matrix[k * sz + j];
+    //         if (matrix[i * sz + j] > distance)
+    //         {
+    //           matrix[i * sz + j] = distance;
+    //         }
+    //       }
+    //     });
+    //   }
+    // }
 
-    // + graph specific optimization
-    // + vectorization
-    [Benchmark]
-    [ArgumentsSource(nameof(Arguments))]
-    public void FloydWarshall_03(int[] matrix, int sz)
-    {
-      for (var k = 0; k < sz; ++k)
-      {
-        for (var i = 0; i < sz; ++i)
-        {
-          if (matrix[i * sz + k] == Constants.NO_EDGE)
-          {
-            continue;
-          }
+    // // + graph specific optimization
+    // // + vectorization
+    // [Benchmark]
+    // [ArgumentsSource(nameof(Arguments))]
+    // public void FloydWarshall_03(int[] matrix, int sz)
+    // {
+    //   for (var k = 0; k < sz; ++k)
+    //   {
+    //     for (var i = 0; i < sz; ++i)
+    //     {
+    //       if (matrix[i * sz + k] == Constants.NO_EDGE)
+    //       {
+    //         continue;
+    //       }
 
-          var ik_vec = new Vector<int>(matrix[i * sz + k]);
+    //       var ik_vec = new Vector<int>(matrix[i * sz + k]);
 
-          var j = 0;
-          for (; j < sz - Vector<int>.Count; j += Vector<int>.Count)
-          {
-            var ij_vec = new Vector<int>(matrix, i * sz + j);
-            var ikj_vec = new Vector<int>(matrix, k * sz + j) + ik_vec;
+    //       var j = 0;
+    //       for (; j < sz - Vector<int>.Count; j += Vector<int>.Count)
+    //       {
+    //         var ij_vec = new Vector<int>(matrix, i * sz + j);
+    //         var ikj_vec = new Vector<int>(matrix, k * sz + j) + ik_vec;
 
-            var lt_vec = Vector.LessThan(ij_vec, ikj_vec);
-            if (lt_vec == new Vector<int>(-1))
-            {
-              continue;
-            }
+    //         var lt_vec = Vector.LessThan(ij_vec, ikj_vec);
+    //         if (lt_vec == new Vector<int>(-1))
+    //         {
+    //           continue;
+    //         }
 
-            var r_vec = Vector.ConditionalSelect(lt_vec, ij_vec, ikj_vec);
-            r_vec.CopyTo(matrix, i * sz + j);
-          }
+    //         var r_vec = Vector.ConditionalSelect(lt_vec, ij_vec, ikj_vec);
+    //         r_vec.CopyTo(matrix, i * sz + j);
+    //       }
 
-          for (; j < sz; ++j)
-          {
-            var distance = matrix[i * sz + k] + matrix[k * sz + j];
-            if (matrix[i * sz + j] > distance)
-            {
-              matrix[i * sz + j] = distance;
-            }
-          }
-        }
-      }
-    }
+    //       for (; j < sz; ++j)
+    //       {
+    //         var distance = matrix[i * sz + k] + matrix[k * sz + j];
+    //         if (matrix[i * sz + j] > distance)
+    //         {
+    //           matrix[i * sz + j] = distance;
+    //         }
+    //       }
+    //     }
+    //   }
+    // }
 
-    // + graph specific optimization
-    // + vectorization
-    // + parallel
-    [Benchmark]
-    [ArgumentsSource(nameof(Arguments))]
-    public void FloydWarshall_04(int[] matrix, int sz)
-    {
-      for (var k = 0; k < sz; ++k)
-      {
-        Parallel.For(0, sz, i =>
-        {
-          if (matrix[i * sz + k] == Constants.NO_EDGE)
-          {
-            return;
-          }
+    // // + graph specific optimization
+    // // + vectorization
+    // // + parallel
+    // [Benchmark]
+    // [ArgumentsSource(nameof(Arguments))]
+    // public void FloydWarshall_04(int[] matrix, int sz)
+    // {
+    //   for (var k = 0; k < sz; ++k)
+    //   {
+    //     Parallel.For(0, sz, i =>
+    //     {
+    //       if (matrix[i * sz + k] == Constants.NO_EDGE)
+    //       {
+    //         return;
+    //       }
 
-          var ik_vec = new Vector<int>(matrix[i * sz + k]);
+    //       var ik_vec = new Vector<int>(matrix[i * sz + k]);
 
-          var j = 0;
-          for (; j < sz - Vector<int>.Count; j += Vector<int>.Count)
-          {
-            var ij_vec = new Vector<int>(matrix, i * sz + j);
-            var ikj_vec = new Vector<int>(matrix, k * sz + j) + ik_vec;
+    //       var j = 0;
+    //       for (; j < sz - Vector<int>.Count; j += Vector<int>.Count)
+    //       {
+    //         var ij_vec = new Vector<int>(matrix, i * sz + j);
+    //         var ikj_vec = new Vector<int>(matrix, k * sz + j) + ik_vec;
 
-            var lt_vec = Vector.LessThan(ij_vec, ikj_vec);
-            if (lt_vec == new Vector<int>(-1))
-            {
-              continue;
-            }
+    //         var lt_vec = Vector.LessThan(ij_vec, ikj_vec);
+    //         if (lt_vec == new Vector<int>(-1))
+    //         {
+    //           continue;
+    //         }
 
-            var r_vec = Vector.ConditionalSelect(lt_vec, ij_vec, ikj_vec);
-            r_vec.CopyTo(matrix, i * sz + j);
-          }
+    //         var r_vec = Vector.ConditionalSelect(lt_vec, ij_vec, ikj_vec);
+    //         r_vec.CopyTo(matrix, i * sz + j);
+    //       }
 
-          for (; j < sz; ++j)
-          {
-            var distance = matrix[i * sz + k] + matrix[k * sz + j];
-            if (matrix[i * sz + j] > distance)
-            {
-              matrix[i * sz + j] = distance;
-            }
-          }
-        });
-      }
-    }
+    //       for (; j < sz; ++j)
+    //       {
+    //         var distance = matrix[i * sz + k] + matrix[k * sz + j];
+    //         if (matrix[i * sz + j] > distance)
+    //         {
+    //           matrix[i * sz + j] = distance;
+    //         }
+    //       }
+    //     });
+    //   }
+    // }
   }
 }
